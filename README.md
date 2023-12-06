eb970cdf94fee63c7613fed487428de67456a4da
zohaibiqbal00 <zohaib@mintstargram.com>
2023-12-06 12:23:03 +0500

    route-setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # webdev-flow
+# create next 13 app npx create-next-app@13.3
--- my-app/pages/about/index.tsx
+++ my-app/pages/about/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
   return (
     <div>
-      about
+      about hi
     </div>
   )
 }
